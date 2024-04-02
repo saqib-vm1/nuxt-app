90b4ee462f6f1772fd28d5105c6e20a0c925909d
Mohd Saqib <mohd.saqib@vidyamantra.com>
2024-04-02 10:11:35 +0530

    using the layer methods

--- app.vue
+++ app.vue
@@ -2,5 +2,6 @@
   <div>
     <!-- <NuxtWelcome /> -->
     <HelloWorld />
+    <Add />
   </div>
 </template>
---
+++ components/Add.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Addition of 2 + 3 = {{ 5 }}</h1>
+  <HelloWorld />
+</template>
+
+<script setup>
+  console.log("Add.vue", add(3, 7), subtract(5, 1))
+
+</script>
